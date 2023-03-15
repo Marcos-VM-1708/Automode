44e5e0fa65dea52a55d4097b41f063ca9865be6a
Marcos vinicios pereira de morais <108135910+Marcos-VM-1708@users.noreply.github.com>
2023-03-15 18:19:00 -0300

    ,,,

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import aws
 import time
 import openai
+from joblib import parallel, delayed
 
 start_time = time.perf_counter()
 
 df = aws.data
-openai.api_key = "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"
+openai.api_key = "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"
 def request(messages):
     # api log:
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
@@ -16,9 +17,10 @@ def request(messages):
     # request:
     return [response.choices[0].message.content, response.usage]
 
-cont = 10
 inicio = time.time()
-for i in range(0, len(df), 10):
+def vai_com(i):
+
+    cont = 10
     coment = df[i:i + 10]
     mensagens = [{"role": "system",
                   "content": "preciso que você interplete o sentimento desses textos, retorne apenas positivo, neutro, negativo e 0 caso vc não consigua identificar, escreva o resultado em um formato de lista separado por virgula e sem ponto final "},
@@ -27,27 +29,26 @@ for i in range(0, len(df), 10):
     if len("".join(coment)) > 600:
         try:
             temp = request(mensagens)
-            print(coment)
-            print(f"{temp[0]}\n")
-            print(i)
-            print(cont)
-            print(f"tokens{ temp[1]}")
             cont = cont + 10
 
         except openai.error.InvalidRequestError:
-            print("deu merda aqui nessa poha")
-            continue
+            return "deu merda aqui poha"
+
 
         except ValueError:
-            print("deu merda aqui")
-            continue
+            return"deu merda aqui"
+
 
         except openai.error.RateLimitError:
             print("esperando request :(")
             fim = time.time()
             print(fim - inicio)
             time.sleep(65)
-            continue
+
+        return coment, temp[0], cont
+
+resultado = parallel(n_jobs = -1)(delayed(vai_com)(i)for i in range(0, len(df), 10))
+print(f"{resultado}\n")
 
 end_time = time.perf_counter()
 print(f"Fim do segundo timer. Tempo de execução: {end_time - start_time:+.4f} segundos")
